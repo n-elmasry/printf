360e1bc0f8c24fbee895e0fa139bb72b42e8f517
nada_elmasry <almassrynada411@gmail.com>
2023-09-16 04:57:40 -0700

    task 1

--- task1.c
+++ task1.c
@@ -1,16 +1,54 @@
 #include <stdarg.h>
+#include "main.h"
 #include <stdio.h>
+/**
+*_printf - prints anything
+*@format: format string
+*Return:
+*/
 
+int _printf(const char *format, ...)
+{
+int i, num, count = 0;
+char num_array[20];
+va_list args;
+va_start(args, format);
+
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
 case 'd':
 case 'i':
 {
-int num = va_arg(args, int);
-char num_array[20];
+num = va_arg(args, int);
+
 sprintf(num_array, "%d", num);
-for (int i = 0; num_array[i]; i++)
+for (i = 0; num_array[i]; i++)
 {
 putchar(num_array[i]);
 count++;
 }
 break;
+}
+default:
+putchar('%');
+putchar(*format);
+count += 2;
+break;
+}
+
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
 }

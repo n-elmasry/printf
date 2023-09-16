e64332d4ed067bfb6133104ce149c3e4e45fc096
nada_elmasry <almassrynada411@gmail.com>
2023-09-16 16:41:49 -0700

    task1

--- main.h
+++ main.h
@@ -11,5 +11,5 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_string(char *str);
-int print_int(int num);
+void print_int(int num);
 #endif
--- print_int.c
+++ print_int.c
@@ -6,16 +6,14 @@
  * @num: the integer to be printed
  * Return: the number of characters printed
  */
-int print_int(int n)
-{
-if (n < 0)
-{
-putchar('-');
-n = -n;
-}
-if (n / 10)
-print_int(n / 10);
 
-putchar(n % 10 + '0');
-return(n);
+void print_int(int num)
+{
+if (num < 10)
+{
+putchar(num + '0');
+return;
+}
+print_int(num / 10);
+putchar((num % 10) + '0');
 }

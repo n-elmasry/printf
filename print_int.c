e8a170850e0a5d2824fcb3a225f93350bdea6e2e
nada_elmasry <almassrynada411@gmail.com>
2023-09-16 08:15:36 -0700

    task1

--- print_int.c
+++ print_int.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 #include "main.h"
 
-void print_int(int num)
+int print_int(int num)
 {
-int i = 0, j, digit;
-char num_array[10];
-if (num == 0)
-{
-putchar('0');
-return;
-}
+int i = 0, j;
+char num_array[12];
+int number = num;
 if (num < 0)
 {
 putchar('-');
 num = -num;
 }
+
 while (num > 0)
 {
-digit = num % 10;
-num_array[i++] = '0' + digit;
+num_array[i++] =  num % 10 + '0';
 num /= 10;
 }
 for (j= i - 1; j >= 0; j--)
 {
 putchar(num_array[j]);
 }
+return (number);
 }

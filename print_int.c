d05bd5ff6b282cae9c42617ea8f30eea23da365c
nada_elmasry <almassrynada411@gmail.com>
2023-09-17 15:34:40 -0700

    task1

--- print_int.c
+++ print_int.c
@@ -1,20 +1,31 @@
+#include <limits.h>
 #include "main.h"
-
 /**
-*print_int - prints integrs
-*@num: num
-*Return: i
-*/
-
+ * print_int - prints integers
+ * @num: integer to print
+ * Return: number of characters printed
+ */
 int print_int(int num)
 {
 int i = 0;
 
-if (num < 0)
+if (num == INT_MIN)
 {
-_putchar('-');
-num = -num;
+
+i += _putchar('-');
+num = -(num + 1);
 i++;
+
+if (num / 10)
+i += print_int(num / 10);
+
+_putchar(num % 10 + '1');
+return (i);
+}
+else if (num < 0)
+{
+i += _putchar('-');
+num = -num;
 }
 
 if (num / 10)
